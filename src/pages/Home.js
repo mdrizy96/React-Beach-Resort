7180a8f54f23f028604ff497403502c8e0d06eb1
mdrizy96 <duncanokeno@gmail.com>
2019-09-13 19:52:14 +0300

    Implement the banner component that appears in the middle of the hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 import Hero from '../components/Hero';
+import Banner from "../components/Banner";
 
 const Home = () => {
     return (
-        <Hero />
+        <Hero>
+            <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                <Link to='/rooms' className="btn-primary">
+                    our rooms
+                </Link>
+            </Banner>
+        </Hero>
     )
 };
 
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 import Hero from '../components/Hero'
+import Banner from "../components/Banner";
 
 const Rooms = () => {
     return (
-        <Hero hero="roomsHero" />
+        <Hero hero="roomsHero">
+            <Banner title="our rooms">
+                <Link className="btn-primary" to='/'>return home</Link>
+            </Banner>
+        </Hero>
     )
 }
 
