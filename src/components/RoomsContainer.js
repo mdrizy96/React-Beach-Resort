c1a52bcf46f9dc224ae0a9c4d1a288c81832acb7
mdrizy96 <duncanokeno@gmail.com>
2019-09-14 12:56:34 +0300

    Finish the RoomsList component

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import RoomsList from "./RoomsList";
 import RoomsFilter from "./RoomsFilter";
+import {RoomConsumer} from "../context";
+import Loading from "./Loading";
 
 const RoomsContainer = () => {
     return (
-        <div>
-            Rooms container
-            <RoomsList/>
-            <RoomsFilter/>
-        </div>
+        <RoomConsumer>
+            {
+                (value) => {
+                    const {loading, sortedRooms, rooms} = value;
+                    if (loading) {
+                        return (
+                            <Loading/>
+                        )
+                    }
+                    return (
+                        <>
+                            <RoomsFilter rooms={sortedRooms}/>
+                            <RoomsList rooms={rooms}/>
+                        </>
+                    )
+                }
+            }
+        </RoomConsumer>
     )
-}
+};
 
 export default RoomsContainer
--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import Room from "./Room";
 
-const RoomsList = () => {
+const RoomsList = ({rooms}) => {
+    if (rooms.length === 0) {
+        return (
+            <div className="empty-search">
+                <h3>unfortunately no rooms matched your search parameters</h3>
+            </div>
+        )
+    }
     return (
-        <div>
-            Rooms List
-        </div>
+        <section className="roomslist">
+            <div className='roomslist-center'>
+                {rooms.map(item => {
+                    return <Room room={item} key={item.id}/>
+                })}
+            </div>
+        </section>
     )
-}
+};
 
 export default RoomsList
