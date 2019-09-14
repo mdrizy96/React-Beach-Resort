fcb80b0aeb6f98871d95c29dceedce833cd122f6
mdrizy96 <duncanokeno@gmail.com>
2019-09-14 11:50:05 +0300

    Show the details, info and extras of each room in the SingleRoom page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -46,6 +46,30 @@ export default class SingleRoom extends Component {
                             return <img key={index} src={item} alt={name} />
                         })}
                     </div>
+                    <div className="single-room-info">
+                        <article className="desc">
+                            <h3>details</h3>
+                            <p>{description}</p>
+                        </article>
+                        <article className="info">
+                            <h3>info</h3>
+                            <h6>price : ${price}</h6>
+                            <h6>size : {size} SQFT</h6>
+                            <h6>max capacity : {capacity} {capacity === 1 ? "person" : "people"}</h6>
+                            <h6>pets : {pets ? "pets allowed" : "no pets allowed"}</h6>
+                            <h6>{breakfast && "free breakfast included"}</h6>
+                        </article>
+                    </div>
+                </section>
+                <section className="room-extras">
+                    <h6>Extras</h6>
+                    <ul className="extras">
+                        {extras.map((item, index) => {
+                            return (
+                                <li key={index}>- {item}</li>
+                            )
+                        })}
+                    </ul>
                 </section>
             </>
         )
